9928ce51b2be99d6d7248e8008c83a7ac84a9967
Lim Yan Jun <yanjun_5473@hotmail.com>
2021-04-27 18:49:14 +0800

    fix concatenation

--- Day 1/day-1-2-exercise/main.py
+++ Day 1/day-1-2-exercise/main.py
@@ -1,7 +1,7 @@
 #Fix the code below 👇
 
 print("Day 1 - String Manipulation") # Missing double quotes at beginning
-print("String Concatenation is done with the "+" sign.")
+print('String Concatenation is done with the "+" sign.') # Change start and end double quote to single quote to include the + as a string
 print('e.g. print("Hello " + "world")') # Indentation removed before print()
 print("New lines can be created with a backslash and n.") # Unnecessary open bracket 
 
