b1da0272c3e9fe04a5bab4694cdfc636604d9091
Lim Yan Jun <yanjun_5473@hotmail.com>
2021-04-27 18:23:03 +0800

    exercise 2

---
+++ Day 1/day-1-2-exercise/main.py
@@ -0,0 +1,8 @@
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation") # Missing double quotes at beginning
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")') # Indentation removed before print()
+print("New lines can be created with a backslash and n.") # Unnecessary open bracket 
+
+
