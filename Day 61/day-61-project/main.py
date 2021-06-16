60d70c9021e401d9391ffbadf087e106497a4d52
Lim Yan Jun <yanjun_5473@hotmail.com>
2021-06-16 20:33:06 +0800

    day 61

---
+++ Day 61/day-61-project/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.secret_key = "61v98edsf6bv5df09b8r4tgbb98fgb"
+Bootstrap(app)
+
+class MyForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = MyForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template('login.html', form=form)
+
+
+@app.route("/success")
+def success():
+    return render_template('success.html')
+
+
+@app.route("/denied")
+def denied():
+    return render_template('denied.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
